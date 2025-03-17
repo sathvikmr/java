2ffb520ae2eb0704a89958acc1daf7f677a02fb2
sathvikmr <sathvikmr09@gmail.com>
2025-03-17 15:09:08 +0530

    Create area.java

---
+++ area.java
@@ -0,0 +1,8 @@
+class program {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in)
+      System.out.println("Enter the radius of circle");
+      int radius = sc.NextInt();
+    System.out.println(3.14 * radius);
+  }
+}
