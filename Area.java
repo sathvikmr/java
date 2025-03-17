a331ceaef29a1207bf58a1d0bae4f374b61c287e
sathvikmr <sathvikmr09@gmail.com>
2025-03-17 15:17:22 +0530

    Update Area.java

--- Area.java
+++ Area.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 class Area {
-  public static void main(String[] args) {
+  public static void main (String args[]) {
     Scanner sc = new Scanner(System.in);
       System.out.println("Enter the radius of circle");
       int radius = sc.nextInt();
