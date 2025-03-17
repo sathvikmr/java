c6a7aab4ea34cccd1dddd4c2549fe0c53c249b71
sathvikmr <sathvikmr09@gmail.com>
2025-03-17 15:16:01 +0530

    Update Area.java

--- Area.java
+++ Area.java
@@ -3,7 +3,7 @@ class Area {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
       System.out.println("Enter the radius of circle");
-      int radius = sc.NextInt();
+      int radius = sc.nextInt();
     System.out.println(3.14 * radius * radius);
   }
 }
